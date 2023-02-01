91d656947d0b6e5145dc23a588d0e015d06cb08e
Esra YILDIZ <esrayildiizz@outlook.com>
2023-02-01 14:06:20 +0300

    Add project files.

---
+++ CustomersProject/CustomersProject/Controllers/MusteriController.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomersProject.Controllers
+{
+    public class MusteriController : Controller
+    {
+        private CustomerProjectEntities1 _db = new CustomerProjectEntities1();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var listele = _db.Musteris.ToList();
+            return View(listele);
+        }
+        [HttpGet]
+        public JsonResult FaturaListele(int id)  //
+        {
+            var faturalistele = _db.sp_fatura_tarih(id).FirstOrDefault();
+            return new JsonResult { JsonRequestBehavior = JsonRequestBehavior.AllowGet, Data = faturalistele };
+
+        }
+
+
+
+    }
+}
